f9d3b8afa59b01165a291c2cc6a0d8b36598324e
darkwingdck <dzakharov3819@gmail.com>
2024-03-23 19:43:53 +0300

    feat: handle mark task

--- main.go
+++ main.go
@@ -6,15 +6,15 @@ import (
     "text/template"
 )
 
-type Item struct {
+type Task struct {
     Title   string
     Done    bool
 }
 
 func rootHandler(writer http.ResponseWriter, request *http.Request) {
     tmpl := template.Must(template.ParseFiles("index.html"))
-    context := map[string][]Item {
-	"Items": {
+    context := map[string][]Task {
+	"Tasks": {
 	    {Title: "Don't do this", Done: false},
 	    {Title: "Don't do that", Done: false},
 	    {Title: "Don't do any of that", Done: false},
@@ -23,14 +23,14 @@ func rootHandler(writer http.ResponseWriter, request *http.Request) {
     tmpl.Execute(writer, context)
 }
 
-func addItemHandler(writer http.ResponseWriter, request *http.Request) {
+func addTaskHandler(writer http.ResponseWriter, request *http.Request) {
     title := request.PostFormValue("title")
     if len(title) == 0 {
 	return
     }
-    tmpl := template.Must(template.ParseFiles("item.html"))
-    context := map[string]Item {
-	"Item": {
+    tmpl := template.Must(template.ParseFiles("task.html"))
+    context := map[string]Task {
+	"Task": {
 	    Title: title,
 	    Done: false,
 	},
@@ -38,15 +38,29 @@ func addItemHandler(writer http.ResponseWriter, request *http.Request) {
     tmpl.Execute(writer, context)
 }
 
-func removeItemHandler(writer http.ResponseWriter, request *http.Request) {
+func removeTaskHandler(writer http.ResponseWriter, request *http.Request) {
     tmpl, _ := template.New("t").Parse("")
     tmpl.Execute(writer, nil)
 }
 
+func markTaskHandler(writer http.ResponseWriter, request *http.Request) {
+    title := request.PostFormValue("title")
+    done := request.PostFormValue("done") == "true"
+    tmpl := template.Must(template.ParseFiles("task.html"))
+    context := map[string]Task {
+	"Task": {
+	    Title: title,
+	    Done: done,
+	},
+    }
+    tmpl.Execute(writer, context)
+}
+
 func main() {
     http.HandleFunc("/", rootHandler)
-    http.HandleFunc("/add-item/", addItemHandler)
-    http.HandleFunc("/remove-item/", removeItemHandler)
+    http.HandleFunc("/add-task/", addTaskHandler)
+    http.HandleFunc("/remove-task/", removeTaskHandler)
+    http.HandleFunc("/mark-task/", markTaskHandler)
 
     fileServer := http.FileServer(http.Dir("css"))
     http.Handle("/css/", http.StripPrefix("/css/", fileServer))
